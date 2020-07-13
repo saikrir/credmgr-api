2f52872fc81b705c428b9e186512ccd6cfb3aca3
Sai Katterishetty <rao.saikrishna@gmail.com>
2020-07-12 23:44:56 -0500

    Added More config

---
+++ src/main/java/rao/saikrishna/apps/credmgr/api/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package rao.saikrishna.apps.credmgr.api.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityConfiguration.class);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        logger.info("Security Configured");
+        auth.inMemoryAuthentication().withUser("skrao").password("skrao").roles("ADMIN", "USER").and()
+                .withUser("jaya").password("jaya").roles("USER");
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/adminPing").hasRole("ADMIN")
+                .antMatchers("/ping")
+                .hasAnyRole("ADMIN", "USER").and().formLogin();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
--- src/main/java/rao/saikrishna/apps/credmgr/api/resources/PingResourceController.java
+++ src/main/java/rao/saikrishna/apps/credmgr/api/resources/PingResourceController.java
@@ -1,21 +1,31 @@
 package rao.saikrishna.apps.credmgr.api.resources;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PingResourceController {
+    private static final Logger logger = LoggerFactory.getLogger(PingResourceController.class);
 
     @Value("${credmgr.api.ping-message}")
     private String pingMessage;
 
+    @Value("${credmgr.api.admin-ping-message}")
+    private String adminPingMessage;
 
-    @GetMapping("/ping")
+    @GetMapping(value = "/ping", produces =  MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> ping() {
         return ResponseEntity.ok(pingMessage);
+    }
+
+    @GetMapping(value = "/adminPing", produces =  MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> adminPing() {
+        return ResponseEntity.ok(adminPingMessage);
     }
 
 }
