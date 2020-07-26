bf513fc0baaf1fac48cf1ccc9fff69c3f5655578
Sai Katterishetty <rao.saikrishna@gmail.com>
2020-07-25 23:47:02 -0500

    Added CORS support

---
+++ src/main/java/rao/saikrishna/apps/credmgr/api/config/CorsConfiguration.java
@@ -0,0 +1,32 @@
+package rao.saikrishna.apps.credmgr.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Value("${server.servlet.context-path}")
+    private String apiContextRoot;
+
+    @Value("${credmgr.api.whitelisted-host}")
+    private String allowedDomain;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                String rootPath = apiContextRoot + "/**";
+                registry.addMapping(rootPath)
+                        .allowedOrigins(allowedDomain)
+                        .allowedMethods("PUT", "DELETE", "GET", "POST")
+                        .allowedHeaders("Authorization")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
