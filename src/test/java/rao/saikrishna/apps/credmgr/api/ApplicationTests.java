ef200766401670180efb6720f670b223d487fe7e
skrao <rao.sakrishna@gmail.com>
2020-08-17 23:58:19 -0500

    Added more tests

--- src/test/java/rao/saikrishna/apps/credmgr/api/ApplicationTests.java
+++ src/test/java/rao/saikrishna/apps/credmgr/api/ApplicationTests.java
@@ -1,18 +1,28 @@
 package rao.saikrishna.apps.credmgr.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import rao.saikrishna.apps.credmgr.api.utils.CryptoUtils;
+import rao.saikrishna.apps.credmgr.api.utils.TokenUtils;
 
-//@SpringBootTest
+@SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    private CryptoUtils cryptoUtils;
+
     static {
-        System.setProperty("APP_KEY", "");
-        System.setProperty("DB_USER", "");
-        System.setProperty("DB_PASSWORD", "");
+        System.setProperty("APP_KEY", "skrao");
+        System.setProperty("DB_USER", "web_user");
+        System.setProperty("DB_PASSWORD", "tempid1");
     }
+
 
     @Test
     void contextLoads() {
+        System.out.println("Password " + cryptoUtils.encryptText("dust"));
+        System.out.println("Password " + cryptoUtils.encryptText("bowl"));
     }
 
 }
